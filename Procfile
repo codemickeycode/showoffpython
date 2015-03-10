74e1a31d2204195672a09d70a96581a85517853d
Micaela Reyes <codemickeycode@gmail.com>
2015-03-10 22:32:19 +0800

    Added files for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn showoffpython.wsgi
--- showoffpython/settings.py
+++ showoffpython/settings.py
@@ -84,3 +84,19 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
