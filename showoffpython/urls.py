60c30925c9655ebecf62205e7d3864a6009160d2
Micaela Reyes <codemickeycode@gmail.com>
2015-03-11 01:37:21 +0800

    Added initial post new entry form

--- showoffpython/urls.py
+++ showoffpython/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'showoffpython.views.home', name='home'),
-    url(r'^$', include('blog.urls')),
+    # url(r'^$', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 )
