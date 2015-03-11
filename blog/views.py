02e23a43e1ccfc3daa66166d80f5766555f83e1e
Micaela Reyes <codemickeycode@gmail.com>
2015-03-11 21:08:19 +0800

    Sort post in desc order

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__isnull=False).order_by('published_date')
+    posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_new(request):
